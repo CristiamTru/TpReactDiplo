78deddf4bde67872544f41a834e0c0198e436ce8
CristiamTru <trujillo.cristiam@gmail.com>
2023-05-15 01:11:39 -0300

    guardar alta de producto

--- src/Services/productosService.js
+++ src/Services/productosService.js
@@ -1,5 +1,5 @@
 
-
+import { firestore } from '../Config/firebase';
 
 export async function getAllProductos(){
     return fetch("https://api.mercadolibre.com/sites/MLA/search?q=ipod").then(res=>res.json())
@@ -11,5 +11,5 @@ export function getById(id){
     return fetch(`https://api.mercadolibre.com/items/${id}`).then(res=>res.json())
 }
 export async function create(payload) {
-    //return await firestore.collection("productos").add(payload);
+    return await firestore.collection("productos").add(payload);
   }
